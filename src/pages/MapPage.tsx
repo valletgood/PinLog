c324873d2f69c59b70b66278dca954dd903f0786
parkyh <pyh@cudo.co.kr>
2025-10-15 16:25:30 +0900

    feat: Sidebar 추가

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -5,6 +5,8 @@ import { setCurrentLocation } from '@/redux/slices/mapSlice';
 import { useAppDispatch } from '@/redux/hooks';
 import { Crosshair } from 'lucide-react';
 import { handleCurrentLocation } from '@/util/mapUtil';
+import SearchLocation from '@/components/map/SearchLocation';
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 
 export default function MapPage() {
   const dispatch = useAppDispatch();
@@ -16,13 +18,19 @@ export default function MapPage() {
 
   return (
     <Layout>
-      <Button
-        className="absolute bottom-10 right-10 z-[9999] rounded-full w-10 h-10 shadow-lg bg-white hover:bg-gray-100"
-        onClick={handleClickCurrentLocation}
-      >
-        <Crosshair className="w-8 h-8 text-gray-800" />
-      </Button>
-      <MapView />
+      <SidebarProvider>
+        <SearchLocation />
+        <SidebarInset>
+          <MapView />
+          <Button
+            className="absolute bottom-10 right-10 z-[9999] rounded-full w-10 h-10 shadow-lg bg-white hover:bg-gray-100"
+            variant="default"
+            onClick={handleClickCurrentLocation}
+          >
+            <Crosshair className="w-8 h-8 text-gray-800" />
+          </Button>
+        </SidebarInset>
+      </SidebarProvider>
     </Layout>
   );
 }
